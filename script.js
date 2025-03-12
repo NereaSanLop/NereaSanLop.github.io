27f89059068f53301f7d6deba2c27b4d59885424
Nerea <146009397+NereaSanLop@users.noreply.github.com>
2025-03-12 17:23:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.getElementById("miBoton").addEventListener("click", function() {
     document.getElementById("mensaje").textContent = "¡Has hecho clic en el botón!";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const sidebar = document.getElementById("sidebar");
+
+    menuBtn.addEventListener("click", function () {
+        if (sidebar.classList.contains("hidden")) {
+            sidebar.classList.remove("hidden");
+            sidebar.classList.add("visible");
+        } else {
+            sidebar.classList.remove("visible");
+            sidebar.classList.add("hidden");
+        }
+    });
+});
+
